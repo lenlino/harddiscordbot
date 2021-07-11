b2201f2e071b626f36e9cbba9391bb5835106216
lenlino <65199014+lenlino@users.noreply.github.com>
2021-07-11 14:32:19 +0900

    ヘルプコマンド追加

--- src/main/kotlin/com/lenlino/harddiscordbot/about.kt
+++ src/main/kotlin/com/lenlino/harddiscordbot/about.kt
@@ -3,8 +3,10 @@ package com.lenlino.harddiscordbot
 import com.jagrosh.jdautilities.command.Command
 import com.jagrosh.jdautilities.command.CommandEvent
 import net.dv8tion.jda.api.EmbedBuilder
+import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
+import org.json.JSONString
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
@@ -22,7 +24,7 @@ class about: Command(){
     override fun execute(event: CommandEvent?) {
         if(event?.args?.isEmpty()==false){
             val apiResponse:JSONObject = readJsonFromUrl("https://api.mcsrvstat.us/2/" + event.args)
-            if (apiResponse.getBoolean("online")) {
+            if (apiResponse.getBoolean("online") ) {
                 val players:JSONObject = apiResponse.getJSONObject("players")
                 val motd:JSONObject = apiResponse.getJSONObject("motd")
                 val embed = EmbedBuilder()
@@ -49,25 +51,58 @@ class about: Command(){
 
     }
 
-    @Throws(IOException::class)
-    private fun readAll(rd: Reader): String {
-        val sb = StringBuilder()
-        var cp: Int
-        while (rd.read().also { cp = it } != -1) {
-            sb.append(cp.toChar())
-        }
-        return sb.toString()
+
+}
+
+class mcskin: Command() {
+    init {
+        this.name = "mcskin"
+        this.help = "minecraftのスキンを取得"
     }
 
-    @Throws(IOException::class, JSONException::class)
-    fun readJsonFromUrl(url: String?): JSONObject {
-        val `is` = URL(url).openStream()
-        return try {
-            val rd = BufferedReader(InputStreamReader(`is`, Charset.forName("UTF-8")))
-            val jsonText = readAll(rd)
-            JSONObject(jsonText)
-        } finally {
-            `is`.close()
+    override fun execute(event: CommandEvent?) {
+        if(event?.args?.isEmpty()==false) {
+            val api = readJsonFromUrl("https://api.mojang.com/users/profiles/minecraft/"+event.args)
+            if (!api.has("null")) {
+                val embed = EmbedBuilder()
+                    .setTitle(api.getString("name"))
+                    .setThumbnail("https://crafatar.com/skins/"+api.getString("id"))
+                    .setImage("https://crafatar.com/renders/body/"+api.getString("id"))
+                    .build()
+                event?.reply(embed)
+            } else {
+                event?.reply("ユーザーが存在しません")
+            }
+        } else {
+            event?.reply("ユーザー名/UUIDを入力してください")
         }
+    }
+}
+
+@Throws(IOException::class)
+private fun readAll(rd: Reader): String {
+    val sb = StringBuilder()
+    var cp: Int
+    while (rd.read().also { cp = it } != -1) {
+        sb.append(cp.toChar())
+    }
+    return sb.toString()
+}
+
+@Throws(IOException::class, JSONException::class)
+fun readJsonFromUrl(url: String?): JSONObject {
+    val `is` = URL(url).openStream()
+    return try {
+        val rd = BufferedReader(InputStreamReader(`is`, Charset.forName("UTF-8")))
+        val jsonText = readAll(rd)
+        if (jsonText.isEmpty()) {
+            val emptyjson = "{null:true}"
+            JSONObject(emptyjson)
+        } else {
+            JSONObject(jsonText)
+        }
+
+    } finally {
+        `is`.close()
     }
 }
